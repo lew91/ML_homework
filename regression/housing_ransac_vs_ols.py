ae018ac80b223f01d342b36940bc36ee8bec8d77
lew91 <jakelewnet@gmail.com>
2019-07-15 17:21:45 +0800

    RANSAC vs OLS

--- regression/housing_ransac_vs_ols.py
+++ regression/housing_ransac_vs_ols.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import RANSACRegressor
 from sklearn import datasets
