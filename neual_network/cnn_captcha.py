2651be36599a278b283fd7049da16504cfda599f
lew91 <jakelewnet@gmail.com>
2019-07-19 08:37:05 +0800

    cnn network on captcha

---
+++ neual_network/cnn_captcha.py
@@ -0,0 +1,44 @@
+import numpy as np
+import random
+import string
+from captcha.image import ImageCaptcha
+
+from tensorflow.keras.utils import Sequence
+from tensorflow.keras.models import *
+from tensorflow.keras.layers import *
+
+characters = string.digits + string.ascii_uppercase
+width, heightt, n_len, n_class = 128, 64, 4, len(characters)
+
+
+class CaptchaSequence(Sequence):
+    def __init__(self, characters, batch_size, steps, n_len=4, width=128, height=64):
+        self.characters = characters
+        self.batch_size = batch_size
+        self.steps = steps
+        self.n_len = n_len
+        self.width = width
+        self.height = height
+        self.n_class = len(characters)
+        self.generator = ImageCaptcha(width=width, height=height)
+
+    def __len__(self):
+        return self.steps
+
+    def __getitem__(self, idx):
+        X = np.zeros((self.batch_size, self.height, self.width, 3), dtype=np.float32)
+        y = [np.zeros((self.batch_size, self.n_class), dtype=np.uint8) for i in range(self.n_len)]
+        for i in range(self.batch_size):
+            random_str = ''.join([random.choice(self.characters) for j in range(self.n_len)])
+            X[i] = np.array(self.generator.generate_image(random_str)) / 255.0
+            for j, ch in enumerate(random_str):
+                y[j][i, :] = 0
+                y[j][i, self.characters.find(ch)] = 1
+        return X, y
+
+    
+def decode(y):
+    y = np.argmax(np.array(y), axis=2)[:, 0]
+    return ''.join([characters[x] for x in y])
+
+
